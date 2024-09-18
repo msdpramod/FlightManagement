4b2635eaef69d29dc9be55a9c72a6ab40ae6370a
kuppapramod <kuppapramod@gmail.com>
2024-09-18 09:10:43 +0530

    Welcome to FlightManagment

---
+++ src/test/java/low_level_design/flightmanagement/FlightManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package low_level_design.flightmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
